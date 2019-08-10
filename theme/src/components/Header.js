aeedceeeb6a180f2b14f6a5bf5e659da7eb390d4
alexandra <alexaspalato@gmail.com>
2019-08-10 11:29:38 +0200

    headroom

--- theme/src/components/Header.js
+++ theme/src/components/Header.js
@@ -7,6 +7,7 @@ import Menu from './Menu'
 import SiteBranding from './SiteBranding'
 
 import { slide as SlideMenu } from 'react-burger-menu'
+import Headroom from 'react-headroom'
 
 const Header = () => {
   const data = useStaticQuery(graphql`
@@ -23,75 +24,77 @@ const Header = () => {
   const { title, url } = data.wpgraphql.generalSettings
 
   return (
-    <StyledHeader
-      sx={{
-        bg: `white`,
-        borderBottom: `1px solid #ddd`,
-        '.bm-burger-button': {
-          position: 'relative',
-          width: '20px',
-          height: '20px',
-          right: '36px',
-          // top: '20px',
-          display: ['block', 'block', 'none'],
+    <Headroom>
+      <StyledHeader
+        sx={{
+          bg: `white`,
+          borderBottom: `1px solid #ddd`,
+          '.bm-burger-button': {
+            position: 'relative',
+            width: '20px',
+            height: '20px',
+            right: '36px',
+            // top: '20px',
+            display: ['block', 'block', 'none'],
 
-          ':hover': {
-            cursor: `pointer`,
-            '.bm-burger-bars': {
-              background: t => t.colors.primary,
-              opacity: 1,
+            ':hover': {
+              cursor: `pointer`,
+              '.bm-burger-bars': {
+                background: t => t.colors.primary,
+                opacity: 1,
+              },
             },
           },
-        },
-        '.bm-burger-bars': {
-          background: `#444`,
-          transition: `all .4s ease-in-out`,
-        },
-        'div:nth-child(3)': {
-          display: [`block`, `block`, `none`],
-        },
-      }}
-    >
-      <Container sx={{ py: [3, 1] }}>
-        <Flex sx={{ justifyContent: 'space-between', alignItems: 'center' }}>
-          <SiteBranding title={title} />
-          <div
-            sx={{ display: ['none', 'none', 'block'], variant: `menus.main` }}
-          >
-            <Menu wordPressUrl={url} />
-          </div>
-          <SlideMenu
-            right
-            width={'300px'}
-            sx={{
-              bg: `secondary`,
-              color: `primary`,
-              top: 0,
+          '.bm-burger-bars': {
+            background: `#444`,
+            transition: `all .4s ease-in-out`,
+          },
+          'div:nth-child(3)': {
+            display: [`block`, `block`, `none`],
+          },
+        }}
+      >
+        <Container sx={{ py: [3, 1] }}>
+          <Flex sx={{ justifyContent: 'space-between', alignItems: 'center' }}>
+            <SiteBranding title={title} />
+            <div
+              sx={{ display: ['none', 'none', 'block'], variant: `menus.main` }}
+            >
+              <Menu wordPressUrl={url} />
+            </div>
+            <SlideMenu
+              right
+              width={'300px'}
+              sx={{
+                bg: `secondary`,
+                color: `primary`,
+                top: 0,
 
-              a: {
-                color: `white`,
-              },
-              fontSize: 2,
-              fontFamily: `heading`,
-              '.sub-menu': {
-                pl: 1,
-                '.menu-item': {
-                  fontSize: 1,
+                a: {
+                  color: `white`,
                 },
-              },
-              '.bm-cross-button': {
-                cursor: `pointer`,
-              },
-              '.bm-cross': {
-                bg: `white`,
-              },
-            }}
-          >
-            <Menu wordPressUrl={url} />
-          </SlideMenu>
-        </Flex>
-      </Container>
-    </StyledHeader>
+                fontSize: 2,
+                fontFamily: `heading`,
+                '.sub-menu': {
+                  pl: 1,
+                  '.menu-item': {
+                    fontSize: 1,
+                  },
+                },
+                '.bm-cross-button': {
+                  cursor: `pointer`,
+                },
+                '.bm-cross': {
+                  bg: `white`,
+                },
+              }}
+            >
+              <Menu wordPressUrl={url} />
+            </SlideMenu>
+          </Flex>
+        </Container>
+      </StyledHeader>
+    </Headroom>
   )
 }
 
--- theme/src/components/PostEntryContent.js
+++ theme/src/components/PostEntryContent.js
@@ -1,13 +1,13 @@
 /** @jsx jsx */
-import { jsx, Styled } from "theme-ui"
+import { jsx, Styled } from 'theme-ui'
 
 const PostEntryContent = ({ post, location }) => {
-  const content = location === "single" ? post.content : post.excerpt
+  const content = location === 'single' ? post.content : post.excerpt
   return (
     <Styled.root
       sx={{
         a: {
-          variant: "links.decorated",
+          variant: 'links.decorated',
         },
       }}
       className="entry-content"
